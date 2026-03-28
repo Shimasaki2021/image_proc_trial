0f48c37d8e384e90505ae4092c608d11e8dd36c5
Shimasaki2021 <shimasaki.takamasa@gmail.com>
2026-03-28 11:31:52 +0900

    c++ エッジ検出まで実装

--- proc_ransac_c/src/main.cpp
+++ proc_ransac_c/src/main.cpp
@@ -1,6 +1,35 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
+double calculateMedian(const cv::Mat& src) 
+{
+    cv::Mat flat;
+    src.reshape(1, 1).copyTo(flat); // 1次元配列に変換
+    std::vector<int> vec;
+    flat.copyTo(vec);
+    std::sort(vec.begin(), vec.end()); // ソート
+    return vec[vec.size() / 2]; // 中央値を取得
+}
+
+void extractEdge(const cv::Mat &img_in_g, cv::Mat &img_edge)
+{
+    // https://qiita.com/kotai2003/items/662c33c15915f2a8517e
+    cv::Scalar mean_val, sigma_val;
+    double med_val, sigma;
+    int min_val, max_val;
+
+    med_val = calculateMedian(img_in_g);
+    cv::meanStdDev(img_in_g, mean_val, sigma_val);
+    sigma = sigma_val[0] / 255.0;
+    min_val = (int)std::max(0.0,   (1.0 - sigma) * med_val);
+    max_val = (int)std::min(255.0, (1.0 + sigma) * med_val);
+
+    cv::Canny(img_in_g, img_edge, min_val, max_val);
+    printf("img_out(%d %d) = cv.Canny(img_in_g, img_edge, %d, %d)\n", 
+        img_edge.cols, img_edge.rows, min_val, max_val);
+    return;
+}
+
 int main(int argc, char *argv[]) 
 {
     int ret;
@@ -12,16 +41,19 @@ int main(int argc, char *argv[])
     }
     else
     {
-        // 画像を作成
-        const char *p_img_file = argv[1];
-        cv::Mat image = cv::imread(p_img_file);
+        const char *p_img_file;
+        cv::Mat img_in, img_in_g, img_edge;
 
-        // 文字を描画
-        cv::putText(image, "Hello WSL2 OpenCV", cv::Point(30, 150), 
-                    cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(0, 255, 0), 2);
+        p_img_file = argv[1];
+        img_in = cv::imread(p_img_file);
+
+        // エッジ検出
+        cv::cvtColor(img_in, img_in_g, cv::COLOR_BGR2GRAY);
+        extractEdge(img_in_g, img_edge);
 
         // ウィンドウに表示
-        cv::imshow("Sample Window", image);
+        // cv::imshow("Sample Window", img_in);
+        cv::imshow("Sample Window", img_edge);
         cv::waitKey(0);
 
         ret = 0;
